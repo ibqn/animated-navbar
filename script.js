f35fd2fcbb3696405ecba4b487a2fb1aa4f501f8
Evgeny Bobkin <evgeny.bobkin@stackfuel.com>
2019-10-23 23:14:20 +0200

    complete menu

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 (function() {
+  $( document ).ready(function() {
+    // Your code here.
+  });
 
+  $( window ).on('scroll', function() {
+    console.log('scroll');
+    if ($( window ).scrollTop()) {
+      $( 'nav' ).addClass('black');
+    } else {
+      $( 'nav' ).removeClass('black');
+    }
+  });
 })();
